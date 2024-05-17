73902d435b61efff3f52edc27e55b6b09992ccd2
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-05-17 04:10:32 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5175,
-  }
+  },
+  build: {
+    outDir: 'dist', // Output directory for the build
+    assetsDir: './', // Relative path for assets
+  },
 })
